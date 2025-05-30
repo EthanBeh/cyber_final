a92538c959171e83f8fa8efdb19022b445c49528
EthanBeh <145681416+EthanBeh@users.noreply.github.com>
2025-05-30 12:12:42 +0000

    comment

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+//hello Ethan Be Hairy, I hope you are doing well when you see this message(if you do). As you did to me, I would also remind you to logout. - Eric
+//(I literally don't remember using public windows so I don't know how I didn't log out)
+//I would love to add some Java, but I have work to do
+
 import java.util.Scanner;
 
 public class Main {
